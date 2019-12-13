09fcbdc1414075fff3f5f06a3666362d356e4eaf
Jocoo <xzpnuist@gmail.com>
2019-12-13 17:12:32 +0800

    init commit

---
+++ src/codewars/core.clj
@@ -0,0 +1,58 @@
+(ns codewars.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello World!"))
+
+(defn -main
+  "main"
+  [& args]
+  (println "hello codewars"))
+
+(defn power [base exponent]
+  (if (zero? exponent)
+    1
+    (loop [cnt exponent
+           acc 1]
+      (if (zero? cnt)
+        acc
+        (recur (dec cnt) (*' acc base))))))
+
+(defn row-sum-odd-numbers [row-num]
+  ;; your code here
+  (power row-num 3))
+
+(defn thirt [n]
+  ;; your code here
+  (loop [nr n]
+    (let [tmp (->> nr
+                   pr-str
+                   (map #(- (int %) 48))
+                   reverse
+                   (map * (cycle [1 10 9 12 3 4]))
+                   (reduce +))]
+      (if (= tmp nr)
+        tmp
+        (recur tmp)))))
+
+(defn stock-list [list-of-books list-of-cat]
+  ;; your code
+  (if (every? seq [list-of-books list-of-cat])
+    (let [m (->> list-of-cat
+                 (mapv #(vector % 0))
+                 flatten
+                 (apply hash-map))
+          rmap (reduce
+                (fn [acc item]
+                  (let [cat (->> item first str)
+                        num (->> item (re-find #"\d+") Integer/parseInt)]
+                    (if (contains? acc cat)
+                      (update acc cat + num)
+                      acc)))
+                m
+                list-of-books
+                )]
+      (map #(vector % (rmap %)) list-of-cat))
+    []))
+
---
+++ test/codewars/core_test.clj
@@ -0,0 +1,26 @@
+(ns codewars.core-test
+  (:require [clojure.test :refer :all]
+            [codewars.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
+
+(defn test-assert [act exp]
+  (is (= act exp)))
+
+(deftest a-test1
+  (testing "thirt"
+   (test-assert(thirt 8529), 79)
+   (test-assert(thirt 85299258), 31)
+   (test-assert(thirt 5634), 57)
+   (test-assert(thirt 1111111111), 71)
+   (test-assert(thirt 987654321), 30)
+))
+
+(deftest a-test2
+  (testing "Test 1"
+    (def ur ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"])
+    (def vr ["A" "B" "C" "D"])
+    (def res [["A" 0] ["B" 1290] ["C" 515] ["D" 600]])
+    (is (= (stock-list ur vr) res))))
